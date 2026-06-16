242f9031180af5f4617c07db5db61f43ffce5057
=Gokuldas-dev <=gokuldasshenoy2@gmail.com>
2026-06-17 00:27:09 +0530

    add solution for problem 1.1

---
+++ Problems_and_solution_1/problem_1.1.py
@@ -0,0 +1,7 @@
+# this is a program to check wheather the given number is possitive , negative or zero
+num = float(input("Enter you number : "))
+if num > 0:
+  print("the number is possitive")
+elif num < 0:
+  print("the number is negative")
+else:  print("the number is zero")    
